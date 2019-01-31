1b3de947ee715f25002289f182e08c63bd506dd7
SIIPOLSKA\ibodia <ibodia@sii.pl>
2019-01-31 18:24:21 +0100

    Result models.

---
+++ src/Models/SearchResult.java
@@ -0,0 +1,37 @@
+package Models;
+
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchResult {
+    List<SearchResultItem> Results;
+
+    public SearchResult(Element body) {
+        if (body == null || body.text().toLowerCase().contains("The document has moved")){
+            System.out.println("Body is null");
+        }
+        Results = new ArrayList<>();
+        Elements items = body.select("#res");
+        if (items != null) {
+            Elements resultDivs = items.select("div.g");
+            for (Element div : resultDivs) {
+                SearchResultItem item = new SearchResultItem(div);
+                if (item.SearchedLink.toLowerCase().contains("instagram.") || item.SearchedLink.toLowerCase().contains("ig.")) {
+                    Results.add(new SearchResultItem(div));
+                    System.out.println("Result item: " + div);
+                    System.out.println("_____________________");
+                }
+            }
+        }
+        System.out.println("Results: "+Results.size());
+        System.out.println();
+    }
+
+    public  List<SearchResultItem> getResults() {
+        return Results;
+    }
+}
---
+++ src/Models/SearchResultItem.java
@@ -0,0 +1,15 @@
+package Models;
+
+import org.jsoup.nodes.Element;
+
+public class SearchResultItem {
+    String MainHeader;
+    String SearchedLink;
+    String Description;
+
+    public SearchResultItem(Element div) {
+        MainHeader = div.select("a").first().select("h3").text();
+        SearchedLink = div.select("a").first().attr("href");
+        Description = div.select("div.s").text();
+    }
+}
