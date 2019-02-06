983879e4a071be2e00214b85a050e1f177384240
SIIPOLSKA\ibodia <ibodia@sii.pl>
2019-02-06 19:36:29 +0100

    no message

--- src/Models/SearchResult.java
+++ src/Models/SearchResult.java
@@ -13,25 +13,28 @@ public class SearchResult {
     public SearchResult(Element body) {
 
         Results = new ArrayList<>();
-        Elements items = body.select("#res");
-        Elements businessList = body.select("#rso > div:nth-child(1) > div > div > div:nth-child(2) > div > div:nth-child(4) > div > *");
-        if (businessList != null) {
-            for (Element item : businessList) {
-                Elements el = item.select("span");
-                String galleryName = "";
-                if (el.size() > 1 && el.get(0) != null) {
-                    galleryName = el.get(0).text();
-                }
+        Elements items = body.select("div.section-result-text-content");
+        System.out.println(items.size());
+        System.out.println("Found " + items.size() + "elements");
 
-                String address = "";
-                if (el.size() > 2 && el.get(el.size() - 2) != null) {
-                    address = el.get(el.size() - 2).text();
-                }
-
-                SearchResultItem resultItem = new SearchResultItem(galleryName, address, "");
-                Results.add(resultItem);
-            }
-        }
+//        Elements businessList = body.select("#rso > div:nth-child(1) > div > div > div:nth-child(2) > div > div:nth-child(4) > div > *");
+//        if (businessList != null) {
+//            for (Element item : businessList) {
+//                Elements el = item.select("span");
+//                String galleryName = "";
+//                if (el.size() > 1 && el.get(0) != null) {
+//                    galleryName = el.get(0).text();
+//                }
+//
+//                String address = "";
+//                if (el.size() > 2 && el.get(el.size() - 2) != null) {
+//                    address = el.get(el.size() - 2).text();
+//                }
+//
+//                SearchResultItem resultItem = new SearchResultItem(galleryName, address, "");
+//                Results.add(resultItem);
+//            }
+//        }
         if (items != null) {
             Elements resultDivs = items.select("div.g");
             for (Element div : resultDivs) {
