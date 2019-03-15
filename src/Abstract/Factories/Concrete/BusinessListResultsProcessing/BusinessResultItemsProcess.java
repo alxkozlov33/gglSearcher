448cf6329d4912f2a1b527d1955c18d873be15b3
Ihor <ihorbodia@gmail.com>
2019-03-15 08:46:05 +0100

    code

--- src/Abstract/Factories/Concrete/BusinessListResultsProcessing/BusinessResultItemsProcess.java
+++ src/Abstract/Factories/Concrete/BusinessListResultsProcessing/BusinessResultItemsProcess.java
@@ -11,14 +11,18 @@ public abstract class BusinessResultItemsProcess {
 
     Elements findBussinessGeoDataByNames(Elements names) {
         Elements addressesElements = new Elements();
-        for (Element element: names) {
+        for (Element element : names) {
             if (element.parent().childNodeSize() == 4 || element.parent().childNodeSize() == 5) {
                 addressesElements.add(element.parent().child(2).select("span").first());
             } else if (element.parent().childNodeSize() == 2) {
                 addressesElements.add(element.parent().child(1).child(1).select("span").first());
             } else if (element.parent().childNodeSize() == 3) {
                 if (element.parent().child(2).childNodeSize() == 1) {
-                    addressesElements.add(element.parent().child(2).child(1).select("span").first());
+                    if (element.parent().child(2).child(1).childNodeSize() == 1) {
+                        addressesElements.add(element.parent().child(2).child(0).select("span").first());
+                    } else if (element.parent().child(2).child(1).childNodeSize() > 1) {
+                        addressesElements.add(element.parent().child(2).child(1).select("span").first());
+                    }
                 } else if (element.parent().child(2).childNodeSize() == 2) {
                     if (element.parent().child(2).child(1).childNodeSize() == 0) {
                         addressesElements.add(new Element("<div>No data</div>"));
