287c8dd375e4c402d09ecb7ea9b5f01fb0881648
Ihor <ihorbodia@gmail.com>
2019-05-19 17:10:47 +0200

    Cherry pick 1.

--- src/Abstract/Engines/CustomProxyMapsClient.java
+++ src/Abstract/Engines/CustomProxyMapsClient.java
@@ -41,8 +41,9 @@ public class CustomProxyMapsClient extends BaseEngine {
                 } while (i < 20);
                 return placeCards;
             } catch (Search.IncorrectPageException ex) {
-                Logger.tag("SYSTEM").error("Incorrect HTML page");
-                Logger.error("Incorrect HTML page \r\n" + ex.getPageSource());
+                Logger.tag("SYSTEM").error("HTML with problems. \r\n");
+                Logger.error(ex.getPageSource());
+                return placeCards;
             } catch (Exception e) {
                 Logger.tag("SYSTEM").info("Attempt: " + i);
                 Logger.tag("SYSTEM").error("Cannot get maps page source, waiting for next attempt: " + requestData.requestURL + " \nCause: " + e.getMessage());
